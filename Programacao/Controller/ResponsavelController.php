77fc9c85504be8ef6395e88848f5306baaa0dac5
Ana Carolina <a.carolina.zumarinho@gmail.com>
2022-05-29 09:33:46 -0300

    Novo arquivo de login

--- Programacao/Controller/ResponsavelController.php
+++ Programacao/Controller/ResponsavelController.php
@@ -5,9 +5,11 @@
     // require 'path/to/PHPMailer/src/PHPMailer.php';
     // require 'path/to/PHPMailer/src/SMTP.php';
     // require 'lib/autoload.php';
+    
     include_once "Model/conexaoBD.php";
     include_once "Model/responsavel.php";
     $dados = filter_input_array(INPUT_POST, FILTER_DEFAULT);
+
     // Permissão para que ambientes externos acessem a configuração de conexão com o banco
     header('Access-Control-Allow-Origin: *');
 
@@ -70,65 +72,6 @@
 
             //"Senha redefinida com sucesso" -> Setar mensagem no construct
         }
-
-        /*function RecuperarSenha()
-        {
-            $responsavel = new Responsavel();
-            $dadosResponsavel = $responsavel->RecuperarSenha();
-
-            //Se o usuário já possuir cadastro no banco de dados, a recuperação de senha pode ser feita
-            if ($responsavel->Cadastrar())
-            {
-                // Se o usuário optar por receber o código de recuperação de senha via e-mail...
-                if ($responsavel->email)
-                {
-                    $responsavel->idResponsavel = $_GET["idResponsavel"];
-                    $responsavel->email = $_GET["email"];
-
-                    // Gerando código aleatório criptografado de verificação contendo 4 dígitos
-                    $responsavel->codigoVerificacao = random_bytes(1000, 9999);
-
-                    // Colocando em execução a função necessária para o envio de email
-                    $responsavel->email = new PHPMailer(true);
-
-                    try
-                    {
-                        // Configurações com o servidor SMTP
-                        $responsavel->email->isSMTP();                                   //Protocolo SMTP para envio de e-mail
-                        $responsavel->email->Servidor       = '';                        
-                        $responsavel->email->SMTPAuth   = true;                          //Autenticação do protocolo
-                        $responsavel->email->Username   = '';                            
-                        $responsavel->email->Password   = '';                            //SMTP password
-                        $responsavel->email->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;   //Enable implicit TLS encryption
-                        $responsavel->email->Porta       = 587;                          //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
-                        
-                        // Definindo autor do e-mail e pegando destinatário digitado pelo usuário
-                        $responsavel->email->setFrom('PsicoKids@gmail.com', 'Equipe PsicoKids');
-                        $responsavel->email->addAddress($responsavel->email["email"]); 
-
-                        // Definindo dados do e-mail                             
-                        $responsavel->email->Assunto = 'Recuperação de senha';
-                        $responsavel->email->Subtitulo = "Olá" . $responsavel->email["email"];
-                        $responsavel->email->Descricao = "Para redefinir sua senha de login em sua conta PsicoKids, copie e cole este código de confirmação: <br>" .$responsavel->codigoVerificacao;
-                        // Chave para verificar se o usuário verificou o email
-
-                        // Executa a ação de enviar o e-mail
-                        $responsavel->email->send();
-                    }
-
-                    catch (Exception $erro)
-                    {
-                        $erro = ['erro' => true];
-                    }
-
-                if ($responsavel->telefone)
-                {
-                    
-                }
-                    
-                }    
-            }
-        }*/
 
         function recuperarSenha()
         {
