1db7e7d2af6348570ad8d69f23885794f289adce
Ana Carolina <a.carolina.zumarinho@gmail.com>
2022-06-09 15:23:24 -0300

    Repositório atualizado

--- login.php
+++ login.php
@@ -3,12 +3,13 @@
     include_once 'conexao.php';
 
     // Pegando valores dos campos declarados no construct
-    $senhaAcesso = $_GET['senhaAcesso'];
+    $email = $_GET['email'];
+    $senha = $_GET['senha'];
 
-    /* Verificando se a senha de acesso digitada pelo usuário existe no banco.
-    Enquanto a senha digitada for existente em algum registro do banco, o usuário é autenticado*/
+    /* Verificando se o e-mail e senha digitados pelo usuário existem no banco.
+    Enquanto o email e senha digitados forem igual aos existentes em registro no banco, o usuário é autenticado*/
     
-    $responsavel = "SELECT idResponsavel FROM responsavel WHERE senhaAcesso='".$senhaAcesso."'";
+    $responsavel = "SELECT id FROM cadastro WHERE email='".$email."' AND senha='".$senha."'";
     
     $responsavel = str_replace("\'","",$responsavel);
     $resultado = mysqli_query($conexao,$responsavel);
@@ -16,7 +17,7 @@
     // Enquanto o banco retornar linha afetada da busca do e-mail e senha digitados, o id é retornado e o usuário permanece logado
     while($linha = mysqli_fetch_array($resultado))
     {
-        echo $linha['idResponsavel'];
+        echo $linha['id'];
     }
 
     // Fecha conexão com o banco
